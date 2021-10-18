565dd9be056a12e4ba67e37e5cb108e19996e6b4
TimmieO <timmieoskarsson01@gmail.com>
2021-10-18 17:59:21 +0200

    Added home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,9 +5,6 @@ export default function Home() {
   return (
     <div className="body">
       <h1>Hello</h1>
-      <a href="/login">Sign In</a>
-      <a href="/register">Sign Up</a>
-      <a href="/auth">Auth</a>
     </div>
   );
 }
