96c7505cf122befcbe4460fc810b32a5377006a8
TimmieO <timmieoskarsson01@gmail.com>
2021-10-18 15:08:22 +0200

    added 2FA and access api call

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,21 @@ const mysql = require("mysql");
 const port = 4000;
 const host = "localhost";
 const jwt = require('jsonwebtoken');
-
 var CryptoJS = require("crypto-js");
+
+const speakeasy = require('speakeasy')
 
 require('dotenv').config({path: '../.env'});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+const userData = {
+  user_id: null,
+  username: null,
+  secret: null,
+  loggedIn: false
+  }
 
 //Handle user
 app.post('/api/user/:action', function(req,res){
@@ -23,14 +31,15 @@ app.post('/api/user/:action', function(req,res){
     case "register":
     {
       action_sql = 'insert into user(firstname, lastname, email, username, password, user_salt) values (?,?,?,?,?,?)';
+      let inset_token_sql = 'insert into token(user_id, token) values (?,?)';
       let pwd_info = encryptPassword(data);
-      registerUser(action_sql, data, pwd_info)
+      registerUser({action_sql, inset_token_sql}, data, pwd_info)
       break;
     }
     case "login":
     {
-      let get_salt_sql = "SELECT user_salt FROM user WHERE user_username = ?";
-      let action_sql = "SELECT user_id, username FROM user WHERE username = ? and password = ?";
+      let get_salt_sql = "SELECT user_salt FROM user WHERE username = ?";
+      let action_sql = "SELECT user.user_id, user.username, token.token FROM user JOIN token ON token.user_id = user.user_id WHERE username = ? and password = ?";
 
       loginUser({get_salt_sql : get_salt_sql, action_sql: action_sql}, data);
 
@@ -48,6 +57,16 @@ app.post('/api/user/:action', function(req,res){
       checkEmail(action_sql, data);
       break;
     }
+    case "validate":
+    {
+      validate(data);
+      break;
+    }
+    case "access":
+    {
+      hasAccess(data);
+      break;
+    }
   }
 
   function checkEmail(sql, data){
@@ -60,6 +79,7 @@ app.post('/api/user/:action', function(req,res){
         return res.send(countTotal);
       }
     })
+    connectionObject.end();
   }
 
   function checkUsername(sql, data){
@@ -72,6 +92,7 @@ app.post('/api/user/:action', function(req,res){
         return res.send(countTotal);
       }
     })
+    connectionObject.end();
   }
 
   function encryptPassword(data){
@@ -87,7 +108,8 @@ app.post('/api/user/:action', function(req,res){
   }
 
   function registerUser(sql, data, pwd_info){
-    connectionObject.query(sql,
+    let authToken = speakeasy.generateSecret().base32;
+    connectionObject.query(sql.action_sql,
       [data.firstname,
         data.lastname,
         data.email,
@@ -97,13 +119,29 @@ app.post('/api/user/:action', function(req,res){
       ],
       function (err, result) {
         if (err) {
-          res.json({message: 'Success', error: true})
+          res.json({message: 'Error', error: true})
         }
         if(!err){
-          res.json({message: 'Success', error: false})
+
+          connectionObject.query(sql.inset_token_sql,
+            [result.insertId,
+              authToken
+            ],
+            function (err, result) {
+            console.log(result);
+              if (err) {
+                res.json({message: 'Error', error: true})
+              }
+              if(!err){
+                res.json({message: 'Success', error: false})
+
+              }
+            }
+          )
         }
-      }
-    )
+        connectionObject.end();
+
+      })
   }
 
   function loginUser(sql, data){
@@ -117,21 +155,26 @@ app.post('/api/user/:action', function(req,res){
           let pwd = data.password;
 
           let hash_pwd = CryptoJS.SHA256(process.env.PUBLIC_SALT + user_salt + pwd).toString(CryptoJS.enc.Hex);
-
           connectionObject.query(sql.action_sql, [data.username, hash_pwd], function (err, result) {
             if (err) {
               console.log(err)
             }
             else {
-              if(result.length > 0){//User exists
+              if(result.length > 0){//User exists'
                 const id = result[0].user_id;
-                const username = result[0].user_username;
-                const token = jwt.sign({id, username}, process.env.ACCESS_TOKEN_SECRET, {
+                const username = result[0].username;
+                const token = result[0].token;
+                const jwtToken = jwt.sign({id, username}, process.env.ACCESS_TOKEN_SECRET, {
                   expiresIn: 600,
                 })
-                return res.json({auth: true, token: token, result});
+                userData.user_id = id;
+                userData.username = username;
+                userData.secret = token;
+                userData.loggedIn = true;
+                userData.validated = false;
+                return res.json({valid: true});
               }else{
-                return res.json({auth: false, message: "Wrong info"});
+                return res.json({valid: false, message: "Wrong info"});
               }
             }
           })
@@ -139,11 +182,110 @@ app.post('/api/user/:action', function(req,res){
           return res.send(result);
         }
       }
+      connectionObject.end();
     })
   }
 
-  connectionObject.end();
+  function validate(data){
+    let token = data.enteredAuthToken;
+    try {
+      const secret = userData.secret;
+
+      const verified = speakeasy.totp.verify({
+        secret,
+        encoding: 'base32',
+        token
+      });
+      if (verified) {
+        userData.validated = true;
+        res.json({verified: true});
+      }
+      if (!verified) {
+        userData.validated = false;
+        res.json({verified: false})
+      }
+    }
+    catch (error){
+    }
+  }
+
+  function hasAccess(data){
+
+    let page = data.path;
+
+    const accessObj = {
+      login: {
+        loggedIn: false,
+        validated: false,
+      },
+      register: {
+        loggedIn: false,
+        validated: false,
+      },
+      auth:{
+        loggedIn: true,
+        validated: false
+      },
+      logout: {
+        loggedIn: true,
+      },
+      home: {
+
+      },
+    }
+
+    if(page == '/login'){
+      if(accessObj.login.loggedIn == userData.loggedIn && accessObj.login.validated == userData.validated){
+        res.json({hasAccess: true});
+        return;
+      }
+      if(userData.loggedIn == false){
+        res.json({hasAccess: true});
+        return;
+      }
+      if(accessObj.login.loggedIn != userData.loggedIn || accessObj.login.validated != userData.validated){
+        res.json({hasAccess: false});
+        return;
+      }
+    }
+    if(page == '/register'){
+      if(accessObj.register.loggedIn == userData.loggedIn && accessObj.register.validated == userData.validated){
+        res.json({hasAccess: true});
+        return;
+      }
+      if(userData.loggedIn == false){
+        res.json({hasAccess: true});
+        return;
+      }
+      if(accessObj.register.loggedIn != userData.loggedIn || accessObj.register.validated != userData.validated){
+        res.json({hasAccess: false});
+        return;
+      }
+    }
+    if(page == '/auth'){
+      if(accessObj.auth.loggedIn == userData.loggedIn && accessObj.auth.validated == userData.validated){
+        res.json({hasAccess: true});
+        return;
+      }
+      if(accessObj.auth.loggedIn != userData.loggedIn || accessObj.auth.validated != userData.validated){
+        res.json({hasAccess: false});
+        return;
+      }
+    }
+    if(page == '/logout'){
+      if(accessObj.logout.loggedIn == userData.loggedIn){
+        res.json({hasAccess: true});
+        return;
+      }
+      if(accessObj.logout.loggedIn != userData.loggedIn){
+        res.json({hasAccess: false});
+        return;
+      }
+    }
+  }
+
 })
+
 
 //Conn to db
 function dbConnection() {
