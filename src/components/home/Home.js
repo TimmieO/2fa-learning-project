88e037f346cb631e3a9e2cafc40cd11ae749b472
TimmieO <timmieoskarsson01@gmail.com>
2021-10-19 18:41:24 +0200

    Added access check (For home, incase 2FA is activated or not)

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,30 @@
 import React, { useState, useEffect } from "react";
 
+import checkAccess from "../../helper/userHasAccess"
+
 export default function Home() {
+
+  const [userHasAccess, setUserHasAccess] = useState()
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    accessCheck();
+  }, [userHasAccess]);
+
+  const accessCheck = async () => {
+    let access = await checkAccess(window.location.pathname)
+    setUserHasAccess(access)
+
+    if(access == true){
+      setLoading(false)
+    }
+  };
 
   return (
     <div className="body">
+      {loading ? "Loading" :
       <h1>Hello</h1>
+      }
     </div>
   );
 }
